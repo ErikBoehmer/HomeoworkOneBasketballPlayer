d021924b0927a3f069190e4f00b9e845a4ac7d0f
ErikBoehmer <43480405+ErikBoehmer@users.noreply.github.com>
2019-03-12 09:16:20 -0500

    Updated version of basketball player 3/6/2019

--- app/src/main/java/com/example/wasco/homeworkonebasketballplayer/LinkedList.java
+++ app/src/main/java/com/example/wasco/homeworkonebasketballplayer/LinkedList.java
@@ -5,12 +5,81 @@ public class LinkedList
     private Node head;
     private Node tail;
     private int count;
+    private int currNode;
+    private int prevNode;
+
 
     public LinkedList()
     {
         this.head = null;
         this.count = 0;
+    }
+    public int removeEnd()
+    {
+        Node n = new Node();
+        if(this.head == null)
+        {
+            this.head = n;
+            this.tail = n;
+        }
+        else
+        {
+            tail.setNextNode(null);
+            tail = n;
 
+        }
+        count--;
+    }
+    public int removeAtIndex()
+    {
+        while (int i = 0; i < index; i++)
+        {
+
+            prevNode = currNode;
+            this.tail.getNextNode();
+
+        }
+    }
+
+    public int removeFront()
+    {
+        Node currNode = this.head;
+        this.head = this.head.getNextNode();
+        currNode.setNextNode(null);
+        this.count --;
+        return currNode.getPayload();
+    }
+    public int getAtIndex(int index)
+    {
+        Node currNode = this.head;
+        for(int i = 0; i < index; i++)
+        {
+            currNode = currNode.getNextNode();
+        }
+        return currNode.getPayload();
+    }
+    public void addAtIndex(int value, int index)
+    {
+        if(index ==0)
+        {
+            this.addFront(value);
+        }
+        else if(this.count == index)
+        {
+            this.addEnd(value);
+        }
+        else
+        {
+            Node currNode = this.head;
+            for(int i = 0; i < index - 1; i++)
+            {
+                currNode = currNode.getNextNode();
+            }
+            Node n = new Node(value);
+            n.setNextNode(currNode.getNextNode());
+            currNode.setNextNode(n);
+            this.count++;
+        }
     }
     public void addEnd(int value)
     {
